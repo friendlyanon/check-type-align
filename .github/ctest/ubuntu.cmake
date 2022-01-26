8d8d54827a8da81c93adfe3f132cd78872c1fd36
friendlyanon <friendlyanon@users.noreply.github.com>
2022-01-26 20:39:45 +0100

    Add some more info to the CI CTest scripts

--- .github/ctest/ubuntu.cmake
+++ .github/ctest/ubuntu.cmake
@@ -1,9 +1,10 @@
 cmake_minimum_required(VERSION 3.14)
 
-get_filename_component(root "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
+get_filename_component(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
+set(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/build")
 
-set(CTEST_SOURCE_DIRECTORY "${root}")
-set(CTEST_BINARY_DIRECTORY "${root}/build")
+set(CTEST_SITE "GitHub Actions")
+set(CTEST_BUILD_NAME Ubuntu)
 
 set(CTEST_USE_LAUNCHERS 1)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
