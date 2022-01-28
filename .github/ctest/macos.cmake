861720d723b5d9be801ecb7c3507eaa7b8512c06
friendlyanon <friendlyanon@users.noreply.github.com>
2022-01-28 16:03:53 +0100

    Revert "Add ::group segments to the CTest scripts"
    This reverts commit d3d0538423c1808f737b0c14eb51f24b712329ef.

--- .github/ctest/macos.cmake
+++ .github/ctest/macos.cmake
@@ -1,6 +1,20 @@
+cmake_minimum_required(VERSION 3.14)
+
+get_filename_component(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
+set(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/build")
+
+set(CTEST_SITE "GitHub Actions")
 set(CTEST_BUILD_NAME MacOS)
 
 set(CTEST_USE_LAUNCHERS 1)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_CONFIGURATION_TYPE Release)
 
-set(OPTIONS -DCheckTypeAlign_DEVELOPER_MODE=1)
+ctest_start(Continuous)
+ctest_configure(RETURN_VALUE ret OPTIONS -DCheckTypeAlign_DEVELOPER_MODE=1)
+if(ret EQUAL "0")
+  ctest_build(RETURN_VALUE ret)
+endif()
+if(ret EQUAL "0")
+  ctest_test()
+endif()
