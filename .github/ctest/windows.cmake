d3d0538423c1808f737b0c14eb51f24b712329ef
friendlyanon <friendlyanon@users.noreply.github.com>
2022-01-28 15:54:59 +0100

    Add ::group segments to the CTest scripts

--- .github/ctest/windows.cmake
+++ .github/ctest/windows.cmake
@@ -1,23 +1,11 @@
-cmake_minimum_required(VERSION 3.14)
-
-get_filename_component(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
-set(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/build")
-
-set(CTEST_SITE "GitHub Actions")
 set(CTEST_BUILD_NAME Windows)
 
 set(CTEST_CMAKE_GENERATOR "Visual Studio 16 2019")
 set(CTEST_CMAKE_GENERATOR_PLATFORM x64)
-set(CTEST_CONFIGURATION_TYPE Release)
 
-ctest_start(Continuous)
-ctest_configure(RETURN_VALUE ret OPTIONS "\
--DCheckTypeAlign_DEVELOPER_MODE=1;\
--DCMAKE_C_FLAGS=/wd5105;\
--DCMAKE_CXX_FLAGS=/wd5105")
-if(ret EQUAL "0")
-  ctest_build(RETURN_VALUE ret)
-endif()
-if(ret EQUAL "0")
-  ctest_test()
-endif()
+set(
+    OPTIONS
+    -DCheckTypeAlign_DEVELOPER_MODE=1
+    -DCMAKE_C_FLAGS=/wd5105
+    -DCMAKE_CXX_FLAGS=/wd5105
+)
